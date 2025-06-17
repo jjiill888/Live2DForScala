7c47ade48bb206ac38d53458714a01f4a3eb3a54
eric <2389946405@qq.com>
2025-06-16 19:35:42 -0500

    Fix JavaFX toolbar

--- modules/examples/javafx/src/main/scala/moe/brianhsu/live2d/demo/javafx/JavaFXMain.scala
+++ modules/examples/javafx/src/main/scala/moe/brianhsu/live2d/demo/javafx/JavaFXMain.scala
@@ -7,17 +7,21 @@ import javafx.stage.Stage
 import moe.brianhsu.live2d.demo.app.DemoApp
 import moe.brianhsu.live2d.demo.javafx.widget.JavaFXAvatarDisplayArea
 import moe.brianhsu.live2d.demo.javafx.widget.JavaFXAvatarDisplayArea.AvatarListener
+import moe.brianhsu.live2d.demo.javafx.widget.JavaFXToolbar
 
 /** Simple JavaFX entry point that will later host the Live2D view. */
 class JavaFXMain extends Application {
   override def start(primaryStage: Stage): Unit = {
     val avatarArea = new JavaFXAvatarDisplayArea
+    val toolbar = new JavaFXToolbar
+
     avatarArea.setAvatarListener(new AvatarListener {
       override def onAvatarLoaded(live2DView: DemoApp): Unit = {}
       override def onStatusUpdated(status: String): Unit = {}
     })
 
     avatarArea.onDemoAppReady { app =>
+      toolbar.setDemoApp(app)
       DemoApp.loadLastAvatarPath() match {
         case Some(path) =>
           app.switchAvatar(path).recoverWith { case _ => app.switchAvatar("def_avatar") }
@@ -27,6 +31,7 @@ class JavaFXMain extends Application {
     }
 
     val root = new BorderPane()
+    root.setTop(toolbar)
     root.setCenter(avatarArea)
     val scene = new Scene(root, 800, 600)
     primaryStage.setTitle("Live2D Scala Demo (JavaFX)")
